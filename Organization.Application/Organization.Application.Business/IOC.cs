136e9239f69c1f73def84bfebe2f8437394547c6
swassouf <sam.wassouf@att.net>
2017-11-20 23:23:42 -0600

    Update application

---
+++ Organization.Application/Organization.Application.Business/IOC.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Unity;
+using Organization.Application.Business.Implementation;
+using Organization.Application.Business.Interface;
+using Organization.Application.Repository.Implementation;
+using Organization.Application.Repository.Interface;
+
+namespace Organization.Application.Business
+{
+    public static class IOC
+    {
+        public static void Initialize(IUnityContainer container)
+        {
+            container = container ?? new UnityContainer();
+
+            //Repository
+            container.RegisterType<IAccountRepository, AccountRepository>();
+            container.RegisterType<IRoleRepository, RoleRepository>();
+            //Business
+            container.RegisterType<IAccountBL, AccountBL>();
+            container.RegisterType<IRoleBL, RoleBL>();
+            container.RegisterType(typeof(ICommonBL<>), typeof(CommonBL<>));
+            container.RegisterType(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
